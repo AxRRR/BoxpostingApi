14eb108c9786dd6f1ddd1c67b8df2cce02ff431c
AxR <alexfabrisiolunatoledo@gmail.com>
2021-06-08 16:58:39 -0500

    First commit BoxPosting

---
+++ src/main/java/com/boxposting/springboot/app/backend/SpringBootDataBoxpostingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.boxposting.springboot.app.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataBoxpostingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataBoxpostingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boxposting/springboot/app/backend/SpringBootDataBoxpostingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boxposting.springboot.app.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDataBoxpostingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
